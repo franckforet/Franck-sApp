5aefd6b55190d1e043c1c79fb230cbe41155bf05
FranckyF <foretf@essilor.fr>
2016-11-23 10:27:24 +0100

    Version corrigé / chapitre Creating-posts

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,27 +1,5 @@
-var postsData = [
-  {
-    title:'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url:'http://meteor.com'
-  },
-  {
-    title: 'The Meteor Book',
-    url: 'http://google.com'
-  }
-];
-
 Template.postsList.helpers({
   posts: function(){
     return Posts.find();
   }
-});
-
-
-Meteor.startup(function() {
-  Tracker.autorun(function() {
-    console.log('There are ' + Posts.find().count() + ' posts');
-  });
 });
