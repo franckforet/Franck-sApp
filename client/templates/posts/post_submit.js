d3533426ba7b06a13269f95bb589f5eff2612002
FranckyF <foretf@essilor.fr>
2016-11-30 16:55:32 +0100

    End Chapter Error

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,13 +20,20 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    Meteor.call('postInsert', post, function(error, result){
-      if(error)
-        return alert(error.reason);
+    var errors = validatePost(post);
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
 
-      if(result.postExists)
-        alert('This link is already use !');
-  });
-        Router.go('postsList');
+    Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return throwError(error.reason);
+
+      // show this result but route anyway
+      if (result.postExists)
+        throwError('This link has already been posted');
+
+      Router.go('postPage', {_id: result._id});
+    });
   }
 });
